172be710e5843ab6b27ad1f6ca5c1984e8bdf42e
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-08-23 09:46:52 +0530

    Initial Commit

---
+++ Login_parse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login_parse
+//
+//  Created by JIJO G OOMMEN on 23/08/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
